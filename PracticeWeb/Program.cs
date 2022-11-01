133a1d6e47a3f733e49f0ac62a25a810623147e5
sdywa <aleliger@protonmail.ch>
2022-11-01 17:48:16 +0300

    add cors

--- PracticeWeb/Program.cs
+++ PracticeWeb/Program.cs
@@ -1,9 +1,20 @@
+using System.IO;
+using PracticeWeb.Services.FileSystemServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy(name: "test", policy => {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    }); 
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<IFileSystemService, FileSystemService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("test");
 
 app.UseHttpsRedirection();
 
