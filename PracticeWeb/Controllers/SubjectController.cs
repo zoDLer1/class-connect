6f0aa0710431a1728dc5546d8af90924052f582d
sdywa <aleliger@protonmail.ch>
2022-11-08 20:18:20 +0300

    add a POST request for adding subjects

--- PracticeWeb/Controllers/SubjectController.cs
+++ PracticeWeb/Controllers/SubjectController.cs
@@ -3,6 +3,8 @@ using PracticeWeb.Exceptions;
 using PracticeWeb.Models;
 using PracticeWeb.Services.FileSystemServices;
 using PracticeWeb.Services.SubjectStorageServices;
+using PracticeWeb.Services.GroupStorageServices;
+using PracticeWeb.Services.ItemStorageServices;
 
 namespace PracticeWeb.Controllers;
 
@@ -12,11 +14,19 @@ public class SubjectController : ControllerBase
 {
     private IFileSystemService _fileSystemService;
     private ISubjectStorageService _subjectStorageService;
+    private IGroupStorageService _groupStorageService;
+    private IItemStorageService _itemStorageService;
 
-    public SubjectController(IFileSystemService fileSystemService, ISubjectStorageService subjectStorageService)
+    public SubjectController(
+        IFileSystemService fileSystemService, 
+        ISubjectStorageService subjectStorageService,
+        IGroupStorageService groupStorageService,
+        IItemStorageService itemStorageService)
     {
         _fileSystemService = fileSystemService;
         _subjectStorageService = subjectStorageService;
+        _groupStorageService = groupStorageService;
+        _itemStorageService = itemStorageService;
     }
 
     [HttpGet]
@@ -41,5 +51,40 @@ public class SubjectController : ControllerBase
         {
             return NotFound();
         }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(string? name, string? groupId, string? description)
+    {
+        if (name == null || groupId == null)
+            return BadRequest();
+
+        var group = await _groupStorageService.GetAsync(groupId);
+        var groupItem = await _itemStorageService.GetAsync(groupId);
+        if (group == null || groupItem == null || groupItem.Type.Name != "Group")
+            return BadRequest();
+
+        try
+        {
+            Item folder = await _fileSystemService.CreateFolderAsync(group.Id, name, 4);
+            var subject = new Subject
+            {
+                Id = folder.Id,
+                GroupId = group.Id,
+                Name = name,
+                Description = description
+            };
+            await _subjectStorageService.CreateAsync(subject);
+        }
+        catch (Exception ex)
+        {
+            if (ex is FolderNotFoundException || ex is ItemNotFoundException)
+                return NotFound();
+            else if (ex is ItemTypeException)
+                return BadRequest();
+            throw;
+        }
+
+        return Ok();
     }
 }
