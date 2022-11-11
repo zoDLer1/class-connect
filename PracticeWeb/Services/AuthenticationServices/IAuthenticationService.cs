ce8d46b1b92d4241d1ca3ed32f582d1548f65a78
sdywa <aleliger@protonmail.ch>
2022-11-11 13:26:41 +0300

    create IAuthenticationService.cs

---
+++ PracticeWeb/Services/AuthenticationServices/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+using PracticeWeb.Models;
+
+namespace PracticeWeb.Services.AuthenticationServices;
+
+/// <summary>
+/// Сервис авторизации пользователей
+/// </summary>
+public interface IAuthenticationService 
+{
+    Task<User> LoginAsync(string email, string password);
+    Task RegisterAsync(
+        string firstName, 
+        string lastName,
+        string patronymic,
+        string email, 
+        string password, 
+        int roleId);
+}
