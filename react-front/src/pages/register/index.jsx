be3f067d9200612b67ed479b28ea2332d64e29bf
zoDLer1 <kuptsov.valya@gmail.com>
2023-02-16 23:30:54 +0300

    created register page

---
+++ react-front/src/pages/register/index.jsx
@@ -0,0 +1,14 @@
+import RegisterForm from "components/forms/register-form"
+import css from './register.module.css'
+
+
+function Register() {
+    return (
+        <div className={css.block}>
+            <RegisterForm />
+        </div>
+        
+    )
+}
+
+export default Register
